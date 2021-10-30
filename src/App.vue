8fc58446f894f9345cfd68c5f02bbadf8fa4016a
yankvasya <yankvasya@gmail.com>
2021-10-30 19:08:11 +0300

    @vue/cli: project deployed

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<Home msg='Hello World'/>
+</template>
+
+<script>
+
+import Home from './components/home-page/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/home-page/Home.vue
@@ -0,0 +1,12 @@
+<template src="./template.html"></template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style src="./style.scss" scoped lang="scss"></style>
