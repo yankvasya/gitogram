7e3cf2a4a757f166d7909b3a12760f2251e2c797
yankvasya <yankvasya@gmail.com>
2021-11-28 18:17:31 +0300

    stories page: readme html + one time fetch readme for story

--- src/store/modules/readme.js
+++ src/store/modules/readme.js
@@ -7,10 +7,20 @@ export default {
     loading: false,
     error: false
   },
-  getters: {},
+  getters: {
+    getReadmeById: (state) => (id) => {
+      return state.data?.find(item => item.id === id)
+    }
+  },
   mutations: {
     SET_README_DATA (state, payload) {
-      state.data = payload
+      if (!state.data) {
+        state.data = [payload]
+      } else {
+        if (!state.data.find(item => item.id === payload.id)) {
+          state.data.push(payload)
+        }
+      }
     },
     SET_README_LOADING (state, payload) {
       state.loading = payload
@@ -20,12 +30,13 @@ export default {
     }
   },
   actions: {
-    async fetchReadme ({ commit }, { id, owner, repo }) {
+    async fetchReadme ({ commit, getters }, { id, owner, repo }) {
+      const currentRepo = getters.getReadmeById(id)
+      if (currentRepo?.content !== undefined) return
       commit('SET_README_ERROR', '')
       commit('SET_README_LOADING', true)
       try {
         const { data } = await api.trandings.getReadme({ owner, repo })
-        console.log(data)
         commit('SET_README_DATA', { id, content: data })
       } catch (e) {
         commit('SET_README_ERROR', `Не удалось загрузить данные, ошибка: "${e}"`)
