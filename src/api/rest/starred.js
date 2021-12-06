de7d397e6b79d96156ae238474385026c9767a22
yankvasya <yankvasya@gmail.com>
2021-12-06 21:35:54 +0300

    corrections

--- src/api/rest/starred.js
+++ src/api/rest/starred.js
@@ -1,17 +1,12 @@
 import { makeRequest } from '../requests'
 
-export const starRepo = ({ owner, repo }) => {
-  console.log(owner, 'owner')
-  console.log(repo, 'repo')
-
-  return makeRequest({
-    url: `/user/starred/${owner}/${repo}`,
-    method: 'PUT',
-    headers: {
-      accept: 'application/vnd.github.v3.html+json'
-    }
-  })
-}
+export const starRepo = ({ owner, repo }) => makeRequest({
+  url: `/user/starred/${owner}/${repo}`,
+  method: 'PUT',
+  headers: {
+    accept: 'application/vnd.github.v3.html+json'
+  }
+})
 
 export const unStarRepo = ({ owner, repo }) => makeRequest({
   url: `/user/starred/${owner}/${repo}`,
